0c6e3625563f38eecae378d6264f26f41c86e9ac
Nuria <nuria.lester@gmail.com>
2020-10-30 18:10:35 +0100

    Initial commit

---
+++ app/src/main/java/di/copiandofamosos/MainActivity.kt
@@ -0,0 +1,13 @@
+package di.copiandofamosos
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
